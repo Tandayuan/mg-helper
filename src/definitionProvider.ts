2fc539236ec96d64a2b25cf2d8ca1c3f8f8305b7
谭达源 <tanleslie@foxmail.com>
2023-08-24 21:51:45 +0800

    #mg-helper实现跳转到case目标——修复踢bug、支持更多跳转情景。

--- src/definitionProvider.ts
+++ src/definitionProvider.ts
@@ -17,9 +17,26 @@ export class MyDefinitionProvider implements DefinitionProvider {
   ): ProviderResult<Definition | LocationLink[]> {
     const lineText = document.lineAt(position.line);
     const text = lineText.text;
-    const reg = /^\s*.*'(\w+[ColumnList|RenderList])'.*$/g;
+    // 获取当前鼠标光标位置position.character，基于位置分别向前和向后遍历截取积累文本，遇到黑名单列表blackList中的元素停止遍历；
+    // 返回最终积累的文本mateText后和reg正则表达式匹配成功表示正确选中目标文本sourceStr。
+    const reg = /^\w+[ColumnList|RenderList]$/g;
+    let mateText = "";
+    const blackList = ['"', "'", ":", " "];
+    let pos = position.character;
+    let pt = text.charAt(pos);
+    while (!blackList.includes(pt) && pos < text.length) {
+      mateText += pt;
+      pt = text.charAt(++pos);
+    }
+    let bos = position.character - 1;
+    let bt = text.charAt(bos);
+    while (!blackList.includes(bt) && bos >= 0) {
+      mateText = bt + mateText;
+      bt = text.charAt(--bos);
+    }
+
     // F12匹配到符合reg规则的文本
-    const sourceStr = reg.test(text) && text.replace(reg, "$1");
+    const sourceStr = reg.test(mateText) && mateText;
     if (sourceStr) {
       let pos = 0;
       // 对整个文档进行遍历, 文档中的每一行文本逐一和sourceStr匹配。
@@ -27,12 +44,17 @@ export class MyDefinitionProvider implements DefinitionProvider {
       while (pos <= document.lineCount) {
         const lineItem = document.lineAt(pos++);
         const lineItemText = lineItem.text;
-        const caseReg = /^\s*.*[case]\s*'(\w+[ColumnList|RenderList])'.*$/g;
-        const mateStr = caseReg.test(lineItemText) && lineItemText.replace(reg, "$1");
+        const caseReg = /^\s*.*[case]\s*['|"](\w+[ColumnList])['|"]\s*[:]\s*.*$/g;
+        const mateStr =
+          caseReg.test(lineItemText) && lineItemText.replace(caseReg, "$1");
         if (mateStr && mateStr === sourceStr) {
+          console.log("mg-helper 跳转成功~");
           return new Location(
             document.uri,
-            new Position(lineItem.lineNumber, lineItemText.indexOf(mateStr) + mateStr.length)
+            new Position(
+              lineItem.lineNumber,
+              lineItemText.indexOf(mateStr) + mateStr.length
+            )
           );
         }
       }
